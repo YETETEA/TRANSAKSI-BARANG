23efef48f986fea91d2379a7f7500a9b5e9822a4
YETETEA <yangtutuaja2025@gmail.com>
2025-10-31 13:54:32 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,3 @@
+// === KONFIGURASI BACKEND ===
+// URL WebApp Apps Script lu:
+const WEBAPP_URL = "https://script.google.com/macros/s/AKfycby7HOzKS-1Z_ZPRQYZW4nBaA5pXyQIIyDOAPWjSFNagONHYGjOaandM_BlCvE5WTe2b/exec";
